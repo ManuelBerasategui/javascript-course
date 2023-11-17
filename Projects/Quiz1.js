3717d71e9c797169c3bc602b8698269aaea38029
ManuelBerasategui <manuelberasategui1@gmail.com>
2023-11-17 17:59:57 -0300

    created question objects

---
+++ Projects/Quiz1.js
@@ -0,0 +1,67 @@
+/* Give introduction to the user at the quiz
+    
+    Ask for user´s name
+    Say hello
+
+    create question array
+    create choices array
+    create correct answers array
+    
+    Show first question
+    Show 3 different (possible) answers
+    prompt the usert to chose an answer (a, b or c)
+    
+    repeat
+
+    give feedback to the user (include number of correct answers, number of incorrect answers and average percentage)
+    
+    */
+
+
+alert('Welcome to the best Quiz ever!!!')
+let name = prompt('First of all...whats your name? ')
+alert('Oh! Hello ' + name + '!!')
+
+let question = [
+    'Wich is the tallest mountain in the world?' = {
+        a: 'Everest',
+        b: 'Aconcagua',
+        c: 'Pilcomayo'
+    },
+    'Which animal is known as the Ship of the Desert?' = {
+        a: 'Sheep',
+        b: 'Camel',
+        c: 'Bird'
+    },
+    'How many days are there in a week?'  = {
+        a: '5',
+        b: '10',
+        c: '7'
+    },
+    'How many hours are there in a day?' = {
+        a: '36',
+        b: '24',
+        c: '100'
+    },
+    'How many letters are there in the English alphabet?' = {
+        a: '26',
+        b: '27',
+        c: '28'
+    },
+    'Rainbow consist of how many colours?' = {
+        a: '7',
+        b: '8',
+        c: '11'
+    },
+
+]
+
+
+
+
+
+
+
+
+
+
