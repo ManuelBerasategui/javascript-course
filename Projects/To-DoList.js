2bb5ac7fa98c20e601aa0d4dfc01a2fedbdf6b71
ManuelBerasategui <140184736+ManuelBerasategui@users.noreply.github.com>
2023-11-18 14:45:21 -0300

    Create To-DoList.js

---
+++ Projects/To-DoList.js
@@ -0,0 +1,39 @@
+/* Pseudocode
+    Create tasks array
+
+    
+    exit the programme = no
+
+    while(exit the programme === no){
+    user's choose = Prompt the user to choose between adding, editing, deleting a task or to exit the programme
+
+    If (user chooses to add a task){
+      task to add auxiliar variable = prompt the user the task to add
+      tasks array += add auxiliar variable
+    }
+
+    Else if (user chooses to edit a task && there are tasks created to edit){
+      prompt the user to choose a task to edit
+      edited task auxiliar variable = prompt the user to edit
+      save the edit auxiliar variable into the task array
+    }
+
+    Else if (user chooses to delete a task && there are tasks created to delete){
+      delete task auxiliar variable = prompt the user wich task to edit
+      if (del auxiliar variable === any variable in main task's array){
+        delete that task in the main task's array
+    }
+    }
+
+    Else if (user chooses to exit the programme){
+      break
+    }
+    else{
+    alert('The user didn't give any type of option')
+    continue
+    }
+
+    
+    }
+    
+    */
