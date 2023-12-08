084933fdfab1def0b5b6af8f40b8b676defc0ceb
ManuelBerasategui <manuelberasategui1@gmail.com>
2023-12-08 13:34:13 -0300

    Continued with Library

--- Projects/library.js
+++ Projects/library.js
@@ -8,6 +8,14 @@ Utiliza prototipos para definir comportamientos comunes compartidos entre difere
     Step 1: Declare objects of books, users and library. Include in books the name, author and due date. In the users object don't forget name and ID. Try to include an array of books and users inside the library object. 
     Step 2: Set up methods. Use prototypes to create borrow book and hand in methods. More if you want. Also include due dated books method.
     Step 3: (Optional) give functionality to your program and include prompts to users to make interactable
+    Step 4: Ask if the user is loged in
+        - If the user is loged in, ask for his name, wich will be a key in the users object
+        - If the user is not loged in, ask for his name and create a key with a random value, wich will be his ID
+        - Once the user is already loged in, you shouldn't ask again for logging in. Unless the page resets.
+    Step 5: Check history of the user
+        - If the user takes out a book, you have to register that somewhere
+        - If the user hands in a book, you must check if the due date is out. And if it is, do something with the user like a tax
+    
 */
 
 /* Pseudocodigo
@@ -43,31 +51,115 @@ Utiliza prototipos para definir comportamientos comunes compartidos entre difere
 
 
 
-var Books = {}
-var Users = {}
-let Library = {}
+var Books = {
+    book1: 'Atomic Habits',
+    book2: '12 Rules of Life',
+    book3: 'Rich Dad Poor Dad',
+    book4: 'The richest man in Babylon',
+    book5: 'The inteligent Investor'
 
-Library.prototype.borrow() = function(){
-    let bookBorrowed = prompt('Choose a book to borrow')
-    alert('Borrowed ' + bookBorrowed)
 }
 
-Library.prototype.hand() = function () {
-    let handed = prompt('Wich book will the user hand in? ')
-    alert('Handed in ' + handed + ' succesfully')
-  }
+var borrowedBooks = {}
 
+function toStringObject(obj){
+    let result = ''
+    for(let key in obj){
+        if(obj.hasOwnProperty(key)){
+        }
+        result += key + ': ' + obj[key] + '\n' 
+    }
+    return result
+
+
+}
+var Users = {
+    Manuel: Math.floor(Math.random() * 1000)
+}
+var libraryObject = {}
+
+function Library(){}
+
+let userName = prompt('Can you give me your users name? ')
+Library.prototype.borrow = function(){
+    let borrowedBook = prompt('Choose a book to borrow ')
+
+    let bookExists = Object.values(Books).includes(borrowedBook)
+    if(bookExists){
+        let userID = Users[userName]
+        let currentDate = new Date()
+        borrowedBooks[userID] = borrowedBook
+        alert('Borrowed ' + borrowedBook + ' at ' + currentDate + '\n' + 'Remember to hand in the book before the day ends')
+    }
+    else{
+        alert('That book does not exist')
+    }
+    
+
+}
+
+Library.prototype.hand = function() {
+    let handed = prompt('Wich book will the user hand in? ')
+    
+    if(currentDate === Date()){
+        alert('Handed in ' + handed + ' succesfully!')
+    }
+    else{
+        alert('The date has expired, you will not be able to borrow a book anymore')
+        return 0
+    }
+    
+}
+
+var libraryInstance = new Library()
 var exit = false
 
+var logedIn = false
+while(logedIn === false){
+    let loged = prompt('Are you already loged in? "yes" or "no"')
+    var exitUser = false
+        if(loged === 'yes'){
+            while(exitUser != true){
+                
+                if(Users.hasOwnProperty(userName)){
+                    alert('Welcome back ' + userName + '!')
+                    exitUser = true
+                }
+                if(Users.hasOwnProperty(userName) === false){
+                    alert('Could not find user')
+                    userName = prompt('Please, tell us your user name')
+                    Users[userName] = Math.floor(Math.random() * 1000)
+                    alert('Good to see you ' + userName)
+                    exitUser = true
+                }
+            }
+            logedIn = true
+        }
+        else if(loged === 'no'){
+            let userName = prompt('Please, give tell me, wich would you like it to be your users name? ')
+            Users[userName] = Math.floor(Math.random() * 1000)
+            alert('Good to see you ' + userName + '!')
+            logedIn = true
+        }
+        else if(loged != 'yes' && loged != 'no'){
+            prompt('That is not an option')
+            logedIn = false
+        }
+    }
+
 while(exit === false){
+
+    alert('This is the Librarys catalog: ' + '\n' + toStringObject(Books))
+
     let option = prompt('Choose an option : 1 "Borrow a book" 2 "Hand in a book" 3 "Exit"')
-    if(option === 1){
-        Library.borrow()
+    if(option === "1"){
+        libraryInstance.borrow()
     }
-    else if(option === 2){
-        Library.hand()
+    else if(option === "2"){
+        libraryInstance.hand()
     }
-    else if(option === 3){
+    else if(option === "3" || option === undefined){
+        console.log('undefined log')
         exit = true
         break
     }
